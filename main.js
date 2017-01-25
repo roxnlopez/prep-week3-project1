1f82e7a0467f5054f2622f691a00024e503434cb
roxnlopez <roxnlopez@gmail.com>
2017-01-24 19:26:07 -0700

    Connected JS to HTML
    connects JS page to HTML page.
    changed status from bad to good.

--- main.js
+++ main.js
@@ -12,29 +12,30 @@ BASIC TRACK: 1-10
 ADVANCED TRACK: 11-15
 =======================================================
 */
+document.getElementById("q0").innerHTML = "JS Page Connected Properly!";
+document.getElementById("q0").classList.add("status-good");
 
 
-
-// 1. Create a function that simply returns a string that says something funny 
+// 1. Create a function that simply returns a string that says something funny
 //    when it is called and then displayed into the HTML page.
 
 
 
-// 2. Define a function called "divideByTwo". 
+// 2. Define a function called "divideByTwo".
 //    It should accept one parameter within a text field called "number" and
-//    submitted when a button is clicked. 
-//    The function should divide the number by two and 
+//    submitted when a button is clicked.
+//    The function should divide the number by two and
 //    then displayed into the HTML page.
 
 
 
-// 3. Define a function called "greeting". 
+// 3. Define a function called "greeting".
 //    It should accept two parameters within input fields, which will both be first names.
 //    The function should display to the HTML page a greeting to both people.
 
 
 
-// 4. Create a function that finds the average of 6 numbers passed in when called 
+// 4. Create a function that finds the average of 6 numbers passed in when called
 //    and returns the value and is then displayed in the HTML page.
 
 
@@ -42,7 +43,7 @@ ADVANCED TRACK: 11-15
 
 // 5. You use Amazon Subscribe & Save to have six cans of cashews automatically sent to you each month.
 //    Write a function that takes the price per unit and calculates the total for you each month.
-//    Since the price of the cashews can change anytime, pass that amount into the function to get your total price. 
+//    Since the price of the cashews can change anytime, pass that amount into the function to get your total price.
 
 
 
@@ -74,14 +75,14 @@ ADVANCED TRACK: 11-15
 
 
 
-// 10. Write a function that accepts the following array and separates the people into two teams. Display each team within the HTML page. 
+// 10. Write a function that accepts the following array and separates the people into two teams. Display each team within the HTML page.
 //    No names next to each other in the array should be on the same team.
      teammates = ["Harold", "Bob", "Sue", "Grady", "Notorious B.I.G.", "JavaCrypt", "Weird guy in the back", "Gary", "Carol", "Hipster Tim", "Janet"]
 
 
 
 
-// 11. Allow a user to enter a three digit number. 
+// 11. Allow a user to enter a three digit number.
 //     Write a function that adds the numbers together.
 //     Hint #1: You need to turn a string into an integer.
 //     Hint #2: Strings can be treated as arrays too.
@@ -90,7 +91,7 @@ ADVANCED TRACK: 11-15
 
 
 
-// 12. You've finally gotten around to counting the change in your piggy bank. 
+// 12. You've finally gotten around to counting the change in your piggy bank.
 //     Write a function that accepts four parameters from a user (quarters, dimes, nickels, and pennies).
 //     The function should take each number of coins and multiply it times each coin's value.
 //     Finally, it should return the total amount of change you have in the following format: "$32.77"
@@ -131,7 +132,7 @@ ADVANCED TRACK: 11-15
 
 // C. Create a die rolling function that accepts no parameters.
 //     It rolls two six-sided-dice, adds the two numbers together, and returns a roll value.
-//     Display the result in the HTML page.   
+//     Display the result in the HTML page.
 //     To get the random number rolled by each die, use Math.random and Math.floor.
 
 
@@ -141,7 +142,7 @@ ADVANCED TRACK: 11-15
 
 
 
-// E. Write a function that takes a year from a user 
+// E. Write a function that takes a year from a user
 //    and reports whether or not it is a leap year.
 //    Display the result in the HTML page.
 //    Remember, a leap year occurs:
@@ -149,11 +150,3 @@ ADVANCED TRACK: 11-15
 //        Except every year that is evenly divisible by 100
 //        Unless the year is also evenly divisible by 400
 //        For example, 1997 is not a leap year, but 1996 is. 1900 is not a leapyear, but 2000 is.
-
-
-
-
-
-
-
-
